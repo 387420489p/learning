9b42cf92783171ae6c896a4a6d0c73f9074542c4
PTR <majercsikpeter@gmail.com>
2022-02-23 20:59:50 +0100

    translated variables to english

--- mocking_spongebob.py
+++ mocking_spongebob.py
@@ -1,11 +1,11 @@
 #ThIs iS GoNnA TrAnSfOrM YoU TeXt tO MoCkInG SpOnGeBoB FoRmAt. PeRfEcT, iF YoU HaVe aN ArGuMeNt oNlInE AnD YoU ArE LoSiNg.
-def mocking_spongebob(szoveg):
-    mocking_szoveg = ""
-    for i in range(len(szoveg)):
+def mocking_spongebob(text):
+    mocking_text = ""
+    for i in range(len(text)):
         if i % 2 == 0:
-            mocking_szoveg += szoveg[i].upper()
+            mocking_text += text[i].upper()
         else:
-            mocking_szoveg += szoveg[i].lower()
-    return mocking_szoveg
+            mocking_text += text[i].lower()
+    return mocking_text
 
 print(mocking_spongebob(input("wHaT tExT dO yOu WaNt To TrAnSfOrM?: ")))
