cec9f2a8a56de649cec8dd0126cfa8577833d820
PTR <99926447+387420489p@users.noreply.github.com>
2022-05-07 12:22:21 +0200

    mocking facebook bot that answers to his name
    part of a bigger project of a friend of mine.

---
+++ mocking_fb_bot.py
@@ -0,0 +1,19 @@
+def on_message(message, author):
+    """
+        Args:
+            message (fbchat.MessageData)
+            author(models.User.User)
+    """
+
+    formatted = message.text.lower().strip()
+
+    if "@cseh" in formatted or "botond" in formatted:
+        mocking_text = ""
+        for i in range(len(formatted)):
+            if i % 2 == 0:
+                mocking_text += formatted[i].upper()
+            else:
+                mocking_text += formatted[i].lower()
+
+
+        message.thread.send_text(mocking_text)
